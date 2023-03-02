7762abf58ed0f48acd8e99581ace48cac75ffe95
Bruno Bandeira <brunoban.dev@gmail.com>
2023-03-02 09:22:26 +0000

    improving theme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,6 +1,26 @@
 local colorscheme = "tokyonight"
 
+local ok, cs = pcall(require, colorscheme)
+if not ok then
+  return
+end
+
+cs.setup {
+    styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+    },
+}
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
